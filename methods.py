plotting fix and scaling fix

methods.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.spatial.transform import Rotation as R
 from itertools import product, permutations, chain
 
+from tqdm import tqdm
+from mpl_toolkits.mplot3d import axes3d
 
 # get the array of 48 possible orientations for a block
 
@@ -35,15 +37,18 @@ def gen_neighbors():
 def plot_state(state, fc=(0.5,0,0.2,0.5)):
     """ Plots a state specified as a 3D boolean array """
     
-    fig = plt.figure()
+    fig = plt.gcf()
     ax = fig.gca(projection='3d')
-    ax.set_aspect('equal')
+    #ax.set_aspect('equal')
     
     ax.voxels(state, facecolors=fc, edgecolors='k')
-    
+    scaling = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz']);
+    ax.auto_scale_xyz(*[[np.min(scaling), np.max(scaling)]] * 3)
+
+
 def plot_coords(coords):
     """ Plot cubes at the coordinates given by coords (N,3). """
-    ranges = np.ptp(coords, 0) + 1
+    ranges = np.ptp(coords, 0).astype(int) + 1
     state = np.zeros(ranges, bool)
     coords = coords + coords.min(0)   
     state[coords[:,0], coords[:,1], coords[:,2]] = True